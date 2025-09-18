0600358ca95c864e357a293addb80a2eb981bb17
tarunmahi <tarunmahendra007@gmail.com>
2025-09-18 10:51:45 +0530

    db removed

--- db/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const string = 'mongodb+srv://mahi:12345@cluster0.xvigcau.mongodb.net/tarun?retryWrites=true&w=majority&appName=Cluster0'
-
-mongoose.connect(string)
-.then(()=>console.log(`connected to the DB...mongoose.`))
-.catch((err)=>console.log(err))
