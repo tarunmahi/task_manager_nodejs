ee1dfde86120c80cce6ed5464f765518be745d13
tarunmahi <tarunmahendra007@gmail.com>
2025-10-08 19:29:17 +0530

    added task and model

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,21 @@
+const Task = require('../models/tasks');
+
+
 const getallTasks = (req,res)=>{
     res.send(`get all tasks`)
 }
-const createtasks = (req,res)=>{
+const createtasks = async  (req,res)=>{
    // const {name, age} = req.body;
     //const { id, name} = req.params;
-    const name = req.body.name;
+    //const name = req.body.name;
   // res.json(req.body.name)
-  res.send(`created a task ${name}`); 
+ // res.send(`created a task ${name}`); 
   // res.send(`create a task with name ${req.body.name} and age is ${req.body.age}`)
+
+  const task =await Task.create(req.body);
+  res.status(201).send({task})
+  console.log({task});
+
 }
 const getTasks = (req,res)=>{
     //const {id} = req.params;
